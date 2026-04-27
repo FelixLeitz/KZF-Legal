a1df2e2eb9bb64bc2efd10f949c85d6ad5e4bebc
FelixLeitz <146998917+FelixLeitz@users.noreply.github.com>
2026-04-27 12:02:29 +1000

    feat: implement full middleware pipeline with error handling and logging functionalities

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
 const config = require("./env");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(config.MONGODB_URI);
-    console.log("MongoDB connected under URI:", config.MONGODB_URI);
+    logger.info(`MongoDB connected under URI: ${config.MONGODB_URI}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    logger.error(`MongoDB connection error: ${error.message}`);
     process.exit(1); // Exit process with failure
   }
 };
